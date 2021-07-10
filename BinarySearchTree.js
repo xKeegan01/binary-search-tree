4f10c675482304885588f14d08b4270111132dd2
Alexander Stratev <alexanderstratev@Alexanders-MacBook-Pro-2.local>
2021-07-10 10:34:26 -0500

    add search methods

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -92,6 +92,60 @@ class BinarySearchTree {
         this.root = removeNode(this.root, val);
         return true;
     }
+
+//traverse thru tree horizontaly    
+    BreadthFirstSearch() {
+        let data = [];
+        let queue = [];
+        let node = this.root;
+        queue.push(node);
+        while(queue.length) {
+            node = queue.shift();
+            data.push(node.val);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        return data;
+    }
+
+//traverse thru tree verticaly    
+    DepthFirstPreOrder() {
+        let data = [];
+        let current = this.root;
+        const traverse = (node) => {
+            data.push(node.val);
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+        };
+        traverse(current);
+        return data;
+    }
+
+//traverse thru tree verticaly starting at bottom
+    DepthFirstPostOrder() {
+        let data = [];
+        let current = this.root;
+        const traverse = (node) => {
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+            data.push(node.val);
+        };
+        traverse(current);
+        return data;
+    }
+
+//traverse thru tree verticaly in order
+    DepthFirstInOrder() {
+        let data = [];
+        let current = this.root;
+        const traverse = (node) => {
+            if (node.left) traverse(node.left);
+            data.push(node.val);
+            if (node.right) traverse(node.right);
+        };
+        traverse(current);
+        return data;
+    }
 }
 
 let tree = new BinarySearchTree();
